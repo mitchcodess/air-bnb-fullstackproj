e9e255aaba20848f1ab698a642b08d1e130f6785
Mitchell Hu <118404863+mitchcodess@users.noreply.github.com>
2023-09-12 12:26:20 -0500

    Modify Seeder

--- backend/db/migrations/20230802150601-create-spot.js
+++ backend/db/migrations/20230802150601-create-spot.js
@@ -42,7 +42,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(500),
       },
       price: {
         type: Sequelize.DECIMAL,
--- backend/db/models/spots.js
+++ backend/db/models/spots.js
@@ -65,7 +65,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING
     },
     description: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING(500)
     },
     price: {
       type: DataTypes.DECIMAL
