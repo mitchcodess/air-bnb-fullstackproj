e49a624df7fd03724b5f7c3dc907c7021c9385dc
Mitchell Hu <118404863+mitchcodess@users.noreply.github.com>
2023-09-12 12:44:45 -0500

    modiy seeders

--- backend/db/seeders/20230808173302-reviews.js
+++ backend/db/seeders/20230808173302-reviews.js
@@ -16,7 +16,7 @@ module.exports = {
     await Review.bulkCreate([{
       spotId: 1,
       userId: 1,
-      review: "A wonderful house for a family vacation (large family). The pool was huge. I mean huge!!! The zero entry was great for the young kids, and the outdoor seating was spacious around the pool. We never went out to eat, because we didn't want to leave this place. The only time we left was for the short 5 minute walk to the beach. It was so close, that we could go several times a day for a dip in the ocean. It was perfect. The house was beautiful and perfectly set up for large groups.",
+      review: "A wonderful house for a family vacation (large family). The pool was huge. I mean huge!!!",
       stars: 5
     },{
       spotId: 1,
@@ -27,7 +27,7 @@ module.exports = {
     {
       spotId: 2,
       userId: 1,
-      review: "This rental was like going into an actual home where you instantly felt comfortable. The amenities were exactly as advertised and exceeded our expectations. Not only was the house and pool perfect, the beach was a quick and easy walk, and the host was so helpful. He was always very responsive with any questions I had leading up to my stay. So very kind and we couldn't be happier with our stay here. Literally couldn't find one thing I'd want to change! We are already looking forward to returning again.",
+      review: "This rental was like going into an actual home where you instantly felt comfortable. The amenities were exactly as advertised and exceeded our expectations.",
       stars: 5
     },
     {
