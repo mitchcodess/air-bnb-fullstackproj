4109e09e8b41a0ae9eaac2fb0ee39ccd43f9e252
Mitchell Hu <118404863+mitchcodess@users.noreply.github.com>
2023-09-12 12:53:01 -0500

    Typing of avgRating

--- frontend/src/components/UI/ReviewsStars.js
+++ frontend/src/components/UI/ReviewsStars.js
@@ -6,7 +6,7 @@ function ReviewsStars({ avgRating }) {
     <div className="review-star-number__container">
       <i className="fa-solid fa-star"></i>
       
-      <div>{avgRating ? avgRating.toFixed(2) : "New"}</div>
+      <div>{avgRating ? Number(avgRating).toFixed(2) : "New"}</div>
     </div>
   );
 }
